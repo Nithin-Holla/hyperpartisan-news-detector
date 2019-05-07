faa5b1d22c7efc8ae7238e9b3056108784647c06
Nithin_Holla <nithin.holla7@gmail.com>
2019-05-07 13:14:04 +0200

    Pad pack and pack pad

--- model/WordEncoder.py
+++ model/WordEncoder.py
@@ -23,15 +23,25 @@ class WordEncoder(nn.Module):
 
     def forward(self, x, len_x, task):
         assert task in self.tasks
-        embed = self.embedding(x)
-        out, hidden = self.encoder(embed)   # slice and reorder out later
         if task == 'hyperpartisan':
+            embed = self.embedding(x)
+            out, hidden = self.encoder(embed)  # slice and reorder out later
             pre_attn = self.pre_attn(out)
             attn_weights = torch.softmax(pre_attn @ self.context_vector, dim=1)
             attn_weights = attn_weights.expand_as(out)
             sentence_embed = torch.sum(attn_weights * out, dim=1)
             return sentence_embed
         elif task == 'metaphor':
-            pred = self.metaphor_fc(out)
+            embed = self.embedding(x)
+            sorted_lengths, sort_indices = torch.sort(len_x, descending=True)
+            embed = embed[sort_indices, :, :]
+            packed_seq = pack_padded_sequence(embed, sorted_lengths, batch_first=True)
+            out, _ = self.encoder(packed_seq)
+            pad_packed_states, _ = pad_packed_sequence(out, batch_first=True)
+            _, unsorted_indices = torch.sort(sort_indices)
+            pad_packed_states = pad_packed_states[unsorted_indices, :, :]
+            flipped_states = torch.cat((pad_packed_states[:, :, 0:self.hidden_dim],
+                                        torch.flip(pad_packed_states[:, :, self.hidden_dim:], dims=[2])), dim=2)
+            pred = self.metaphor_fc(flipped_states)
             return pred
 
