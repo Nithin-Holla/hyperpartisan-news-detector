bc51a4642a2927985e52485ad98ed274fe9c08fa
john <ioannis.tsiamas@gmail.com>
2019-05-08 14:29:13 +0200

    hyperpartisan is working

--- model/JointModel.py
+++ model/JointModel.py
@@ -10,21 +10,41 @@ class JointModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, hyp_n_classes, pretrained_vectors):
         super(JointModel, self).__init__()
         self.word_encoder = WordEncoder(vocab_size, embedding_dim, hidden_dim, pretrained_vectors)
-        self.sentence_encoder = SentenceEncoder(hidden_dim, hidden_dim)
+        self.sentence_encoder = SentenceEncoder(2*hidden_dim, hidden_dim)
         self.hyperpartisan_fc = nn.Linear(2 * hidden_dim, hyp_n_classes)
         self.tasks = ['hyperpartisan', 'metaphor']
 
     def forward(self, x, len_x, task):
         assert task in self.tasks
         if task == 'hyperpartisan':
-            sent_embeddings = []
-            for sent in x:
-                sent_embed = self.word_encoder(sent, task)
-                sent_embeddings.append(sent_embed)
-            sent_embeddings = torch.stack(sent_embeddings)
-            doc_embedding = self.sentence_encoder(sent_embeddings)
+
+            # len_x argument contains the recover_idx to unsort sentences
+            # and a list of the number of sentences per article to batch them
+            recover_idx, num_sent_per_document = len_x
+            batch_size = len(num_sent_per_document)
+
+            sorted_sent_embeddings = self.word_encoder(x, len_x, task)
+
+            # unsort
+            sent_embeddings_2d = torch.index_select(sorted_sent_embeddings, dim = 0, index = recover_idx)
+
+            max_num_sent = torch.max(num_sent_per_document)
+
+            # create new 3d tensor (already padded across dim=1)
+            sent_embeddings_3d = torch.zeros(batch_size, max_num_sent, sent_embeddings_2d.shape[-1])
+
+            # fill the 3d tensor
+            processed_sent = 0
+            for i, num_sent in enumerate(num_sent_per_document):
+                sent_embeddings_3d[i, :num_sent, :] = sent_embeddings_2d[processed_sent: processed_sent + num_sent, :]
+                processed_sent += num_sent
+
+            # get document embeddings
+            doc_embedding = self.sentence_encoder(sent_embeddings_3d)
+
             out = self.hyperpartisan_fc(doc_embedding)
-            return out
+
         elif task == 'metaphor':
             out = self.word_encoder(x, len_x, task)
-            return out
+
+        return out
