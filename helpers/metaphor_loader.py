c879cbe9d5623cdb6e1b0d448b175d4a6df68632
ktodorov <kztodorov@outlook.com>
2019-05-10 19:31:47 +0200

    Adds lowercase and tokenize options for metaphors

--- helpers/metaphor_loader.py
+++ helpers/metaphor_loader.py
@@ -12,14 +12,15 @@ class MetaphorLoader():
     @staticmethod
     def get_metaphor_datasets(
             metaphor_dataset_folder: str,
-            word_vector: Vectors,
-            elmo_vectors: int) -> Tuple[MetaphorDataset, MetaphorDataset, MetaphorDataset]:
+            glove_vectors: Vectors,
+            lowercase_sentences: bool = False,
+            tokenize_sentences: bool = True) -> Tuple[MetaphorDataset, MetaphorDataset, MetaphorDataset]:
         '''
         Parses the metaphor files and creates MetaphorDataset objects which
         include information about the vocabulary and the embedding of the sentences
 
         :param str metaphor_dataset_folder: The folder where the metaphor dataset files should be
-        :param Vectors word_vector: The vector that will be used to embed the words in the metaphor dataset. It could be GloVe for example
+        :param Vectors glove_vectors: The vector that will be used to embed the words in the metaphor dataset. It could be GloVe for example
         '''
 
         assert os.path.isdir(
@@ -30,20 +31,29 @@ class MetaphorLoader():
             metaphor_dataset_folder, 'VUA_seq_formatted_train.csv')
 
         train_dataset = MetaphorDataset(
-            train_filepath, word_vector, elmo_vectors)
+            filename=train_filepath,
+            glove_vectors=glove_vectors,
+            lowercase_sentences=lowercase_sentences,
+            tokenize_sentences=tokenize_sentences)
 
         # Validation
         validation_filepath = os.path.join(
             metaphor_dataset_folder, 'VUA_seq_formatted_val.csv')
 
         validation_dataset = MetaphorDataset(
-            validation_filepath, word_vector, elmo_vectors)
+            filename=validation_filepath,
+            glove_vectors=glove_vectors,
+            lowercase_sentences=lowercase_sentences,
+            tokenize_sentences=tokenize_sentences)
 
         # Test
         test_filepath = os.path.join(
             metaphor_dataset_folder, 'VUA_seq_formatted_test.csv')
 
         test_dataset = MetaphorDataset(
-            test_filepath, word_vector, elmo_vectors)
+            filename=test_filepath,
+            glove_vectors=glove_vectors,
+            lowercase_sentences=lowercase_sentences,
+            tokenize_sentences=tokenize_sentences)
 
         return train_dataset, validation_dataset, test_dataset
