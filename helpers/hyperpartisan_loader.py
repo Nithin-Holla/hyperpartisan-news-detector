add4f39e33959a78771478f4cdc4e7ba15c8196c
ktodorov <kztodorov@outlook.com>
2019-05-10 20:31:42 +0200

    Adds Elmo configuration options for hyperpartisan

--- helpers/hyperpartisan_loader.py
+++ helpers/hyperpartisan_loader.py
@@ -9,31 +9,47 @@ from typing import Tuple
 
 class HyperpartisanLoader():
 
-	@staticmethod
-	def get_hyperpartisan_datasets(
-			hyperpartisan_dataset_folder: str,
-			word_vector: Vectors,
-			elmo_vectors: int) -> Tuple[HyperpartisanDataset, HyperpartisanDataset]:
-		'''
-		Parses the hyperpartisan files and creates HyperpartisanDataset objects which
-		include information about the vocabulary and the embedding of the sentences
+    @staticmethod
+    def get_hyperpartisan_datasets(
+            hyperpartisan_dataset_folder: str,
+            glove_vectors: Vectors,
+            lowercase_sentences: bool = False) -> Tuple[HyperpartisanDataset, HyperpartisanDataset]:
+        '''
+        Parses the hyperpartisan files and creates HyperpartisanDataset objects which
+        include information about the vocabulary and the embedding of the sentences
 
-		:param str hyperpartisan_dataset_folder: The folder where the hyperpartisan dataset files should be
-		:param Vectors word_vector: The vector that will be used to embed the words in the hyperpartisan dataset. It could be GloVe for example
-		'''
+        :param str hyperpartisan_dataset_folder: The folder where the hyperpartisan dataset files should be
+        :param Vectors glove_vectors: The vector that will be used to embed the words in the hyperpartisan dataset. It could be GloVe for example
+        :param bool lowercase_sentences: Specify whether the sentences should be lowercased before embedding them
+        '''
 
-		assert os.path.isdir(hyperpartisan_dataset_folder), 'Hyperpartisan dataset folder is not valid'
+        assert os.path.isdir(
+            hyperpartisan_dataset_folder), 'Hyperpartisan dataset folder is not valid'
 
-		# Train
-		train_filepath = os.path.join(hyperpartisan_dataset_folder, 'train_byart.txt')
-		train_dataset = HyperpartisanDataset(train_filepath, word_vector, elmo_vectors)
+        # Train
+        train_filepath = os.path.join(
+            hyperpartisan_dataset_folder, 'train_byart.txt')
 
-		# Validation
-		validation_filepath = os.path.join(hyperpartisan_dataset_folder, 'valid_byart.txt')
-		validation_dataset = HyperpartisanDataset(validation_filepath, word_vector, elmo_vectors)
+        train_dataset = HyperpartisanDataset(
+            filename=train_filepath,
+            glove_vectors=glove_vectors,
+            lowercase_sentences=lowercase_sentences)
 
-		# Test
-		# test_filepath = os.path.join(hyperpartisan_dataset_folder, 'test_byart.txt')
-		# test_dataset = HyperpartisanDataset(test_filepath, word_vector, elmo_vectors)
+        # Validation
+        validation_filepath = os.path.join(
+            hyperpartisan_dataset_folder, 'valid_byart.txt')
 
-		return train_dataset, validation_dataset
+        validation_dataset = HyperpartisanDataset(
+            filename=validation_filepath,
+            glove_vectors=glove_vectors,
+            lowercase_sentences=lowercase_sentences)
+
+        # Test
+        # test_filepath = os.path.join(hyperpartisan_dataset_folder, 'test_byart.txt')
+		
+        # test_dataset = HyperpartisanDataset(
+        # 	filename=test_filepath,
+        # 	glove_vectors=glove_vectors,
+        # 	lowercase_sentences=lowercase_sentences)
+
+        return train_dataset, validation_dataset
