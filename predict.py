68560b271bb0f5044437cd79df6d2155af4124cd
Nithin_Holla <nithin.holla7@gmail.com>
2019-05-14 22:03:07 +0200

    Heatmap of attention weights

--- predict.py
+++ predict.py
@@ -3,12 +3,86 @@ import os
 import argparse
 import seaborn as sns
 import numpy as np
+from matplotlib import pyplot as plt
 
 from enums.training_mode import TrainingMode
 from helpers.data_helper_hyperpartisan import DataHelperHyperpartisan
 from helpers.hyperpartisan_loader import HyperpartisanLoader
 from model.JointModel import JointModel
 from train import load_glove_vectors
+
+
+def title_attention_plot(hyperpartisan_validation_dataset, model):
+    _, hyperpartisan_validation_dataloader = DataHelperHyperpartisan.create_dataloaders(
+        validation_dataset=hyperpartisan_validation_dataset,
+        test_dataset=None,
+        batch_size=4,
+        shuffle=False)
+
+    attn_summary = None
+
+    for idx, (article, article_target, article_recover_idx, article_num_sent, article_sent_lengths) in enumerate(
+            hyperpartisan_validation_dataloader):
+        model.eval()
+        with torch.no_grad():
+            pred, _, batch_sent_attn = model.forward(article,
+                                                     (article_recover_idx, article_num_sent, article_sent_lengths),
+                                                     task=TrainingMode.Hyperpartisan, return_attention=True)
+        batch_sent_attn = np.around(batch_sent_attn.numpy(), decimals=4)
+        article_num_sent = article_num_sent.numpy()
+        article_attn = np.zeros((batch_sent_attn.shape[0], 2))
+        article_attn[:, 0] = batch_sent_attn[:, 0]
+        article_attn[:, 1] = np.sum(batch_sent_attn[:, 1:], axis=1) / (article_num_sent - 1)
+        if idx == 0:
+            attn_summary = article_attn
+        else:
+            attn_summary = np.vstack([attn_summary, article_attn])
+    attn_summary = np.mean(attn_summary, axis=0, keepdims=True)
+    plt.figure()
+    sns.heatmap(attn_summary, square=True, annot=True)
+    plt.show()
+
+
+def visualize_article_attention(hyperpartisan_validation_dataloader, model, article_id):
+    """
+    Generates a heatmap of the word-level and attention-level attention weights
+    :param hyperpartisan_validation_dataloader: Dataloader for hyperpartisan validation set
+    :param model: Trained model
+    :param article_id: ID of the article, i.e., the line number in valid_byart.txt
+    :return: None
+    """
+    # Load one article
+    for idx, (article, article_target, article_recover_idx, article_num_sent, article_sent_lengths) in enumerate(
+            hyperpartisan_validation_dataloader):
+        if idx + 2 == article_id:
+            break
+
+    # Obtain prediction and attention weights from the model
+    model.eval()
+    with torch.no_grad():
+        pred, word_attn, sent_attn = model.forward(article,
+                                                   (article_recover_idx, article_num_sent, article_sent_lengths),
+                                                   task=TrainingMode.Hyperpartisan, return_attention=True)
+    pred = pred.item()
+    article_sent_lengths = article_sent_lengths[article_recover_idx]
+    word_attn = np.around(word_attn.numpy(), decimals=4)
+    sent_attn = np.around(sent_attn.numpy(), decimals=4)
+
+    # Display results
+    print('Hyperpartisan score: {}'.format(pred))
+    for sent_idx in range(article_num_sent):
+        print('Sentence {} -> Word-level attention: {}'.format(sent_idx + 1,
+                                                               word_attn[sent_idx, range(article_sent_lengths[sent_idx])]))
+    print('Sentence-level attention: {}'.format(sent_attn))
+
+    # Plot word-level attention weights per sentence
+    plt.figure()
+    sns.heatmap(word_attn)
+
+    # Plot sentence-level attention weights
+    plt.figure()
+    sns.heatmap(sent_attn, square=True)
+    plt.show()
 
 
 def predict(config):
@@ -34,42 +108,19 @@ def predict(config):
     glove_vectors = load_glove_vectors(config.vector_file_name, config.vector_cache_dir, config.glove_size)
 
     # Load the dataset and dataloader
-    hyperpartisan_train_dataset, hyperpartisan_validation_dataset = HyperpartisanLoader.get_hyperpartisan_datasets(
+    _, hyperpartisan_validation_dataset = HyperpartisanLoader.get_hyperpartisan_datasets(
         hyperpartisan_dataset_folder=config.hyperpartisan_dataset_folder,
         glove_vectors=glove_vectors,
         lowercase_sentences=config.lowercase)
 
     _, hyperpartisan_validation_dataloader = DataHelperHyperpartisan.create_dataloaders(
-        train_dataset=hyperpartisan_train_dataset,
         validation_dataset=hyperpartisan_validation_dataset,
         test_dataset=None,
         batch_size=1,
         shuffle=False)
 
-    # Load one article
-    article_id = 2  # Line number as in valid_byart.txt
-    for idx, (article, article_target, article_recover_idx, article_num_sent, article_sent_lengths) in enumerate(
-            hyperpartisan_validation_dataloader):
-        if idx + 2 == article_id:
-            break
-
-    # Obtain prediction and attention weights from the model
-    joint_model.eval()
-    with torch.no_grad():
-        pred, word_attn, sent_attn = joint_model.forward(article,
-                                                         (article_recover_idx, article_num_sent, article_sent_lengths),
-                                                         task=TrainingMode.Hyperpartisan, return_attention=True)
-    pred = pred.item()
-    article_sent_lengths = article_sent_lengths[article_recover_idx]
-    word_attn = np.around(word_attn.numpy(), decimals=4)
-    sent_attn = np.around(sent_attn.numpy(), decimals=4)
-
-    # Display results
-    print('Hyperpartisan score: {}'.format(pred))
-    for sent_idx in range(article_num_sent):
-        print('Sentence {} -> Word-level attention: {}'.format(sent_idx + 1,
-                                                               word_attn[sent_idx, range(article_sent_lengths[sent_idx])]))
-    print('Sentence-level attention: {}'.format(sent_attn))
+    title_attention_plot(hyperpartisan_validation_dataset, joint_model)
+    visualize_article_attention(hyperpartisan_validation_dataloader, joint_model, article_id=2)
 
 
 if __name__ == '__main__':
